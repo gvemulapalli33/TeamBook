5f581c62b50b87c74039b7851f7b53d7a0d16cae
gvemulapalli33 <gvemulapalli33@gmail.com>
2020-12-30 18:24:25 -0800

    update names

--- src/data/teambook-data.js
+++ src/data/teambook-data.js
@@ -1,18 +1,18 @@
 const people = {
   developers: [
-    {id: 1, name: 'Eric', quote: 'You miss all the shots you don\'t take.', goals: 'Most Likely to Win the PGA', photo: './eric.jpg'},
-    {id: 2, name: 'Amy', quote: 'I believe in you, cuties!', goals: 'Most Likely to Be an Actual Superhero', photo: './amy.jpg'},
-    {id: 3, name: 'Khalid', quote: 'Don\'t put the world on your shoulders.', goals: 'Most Likely to Save the Planet from Alien Invasion', photo: './khalid.png'},
-    {id: 4, name: 'Travis', quote: 'You do you.', goals: 'Most Likely to Win a Grammy', photo: './travis.jpg'},
-    {id: 5, name: 'Leta', quote: 'It\'s okay to be kind of a mess.', goals: 'Most Likely to Go to the Moon', photo: './leta.jpg'},
-    {id: 6, name: 'Christie', quote: 'Every journey begins with a single step. So take the damn step!', goals: 'Most Likely to Be President', photo: './christie.jpg'},
-    {id: 7, name: 'Brittany', quote: 'It was Robbie.', goals: 'Most Likely to Get Away with Murder', photo: './brittany.jpg'},
-    {id: 8, name: 'Robbie', quote: 'Cool.', goals: 'Most Likely to Be Accused of Murder', photo: './robbie.jpg'},
-    {id: 9, name: 'Will', quote: 'Stop worrying and just do the thing.', goals: 'Most Likely to Invent Cold Fusion', photo: './will.jpg'},
-    {id: 10, name: 'Pam', quote: 'Shit!', goals: 'Most Likely to Win a Gold Medal', photo: './pam.jpg'},
-    {id: 11, name: 'David', quote: 'I love git.', goals: 'Most Likely to Be Eaten by a Shark', photo: './david.JPG'},
-    {id: 12, name: 'Louisa', quote: 'YOLO', goals: 'Most Likely to Win a Knife Fight', photo: './louisa.jpg'},
-    {id: 13, name: 'Jeff', quote: '::thoughtful nonsense::', goals: 'Most Likely to Tell a Dad Joke', photo: 'https://i.ytimg.com/vi/DhqzMc_LXgQ/maxresdefault.jpg'},
+    {id: 1, name: 'Zikie', quote: 'You miss all the shots you don\'t take.', goals: 'Most Likely to Win the PGA', photo: './eric.jpg'},
+    {id: 2, name: 'Yiang', quote: 'I believe in you, cuties!', goals: 'Most Likely to Be an Actual Superhero', photo: './amy.jpg'},
+    {id: 3, name: 'Geetha', quote: 'Don\'t put the world on your shoulders.', goals: 'Most Likely to Save the Planet from Alien Invasion', photo: './khalid.png'},
+    {id: 4, name: 'Aneesh', quote: 'You do you.', goals: 'Most Likely to Win a Grammy', photo: './travis.jpg'},
+    {id: 5, name: 'Gaurav', quote: 'It\'s okay to be kind of a mess.', goals: 'Most Likely to Go to the Moon', photo: './leta.jpg'},
+    {id: 6, name: 'Disha', quote: 'Every journey begins with a single step. So take the damn step!', goals: 'Most Likely to Be President', photo: './christie.jpg'},
+    {id: 7, name: 'Savitha', quote: 'It was Robbie.', goals: 'Most Likely to Get Away with Murder', photo: './brittany.jpg'},
+    {id: 8, name: 'Ravi', quote: 'Cool.', goals: 'Most Likely to Be Accused of Murder', photo: './robbie.jpg'},
+    {id: 9, name: 'Ram', quote: 'Stop worrying and just do the thing.', goals: 'Most Likely to Invent Cold Fusion', photo: './will.jpg'},
+    {id: 10, name: 'Adhya', quote: 'Shit!', goals: 'Most Likely to Win a Gold Medal', photo: './pam.jpg'},
+    {id: 11, name: 'Adhiti', quote: 'I love git.', goals: 'Most Likely to Be Eaten by a Shark', photo: './david.JPG'},
+    {id: 12, name: 'Sawpna', quote: 'YOLO', goals: 'Most Likely to Win a Knife Fight', photo: './louisa.jpg'},
+    {id: 13, name: 'Wan', quote: '::thoughtful nonsense::', goals: 'Most Likely to Tell a Dad Joke', photo: 'https://i.ytimg.com/vi/DhqzMc_LXgQ/maxresdefault.jpg'},
   ]
 }
 
